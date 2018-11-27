062cf6e8153f5e56fbee41859edc37d9885f9abe
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-11-27 17:01:43 +0000

    Passing Test 6

--- specs/bear_test.rb
+++ specs/bear_test.rb
@@ -7,9 +7,13 @@ require_relative("../river")
 class BearTest < MiniTest::Test
 
   def test_get_bear_name
-    bear = Bear.new("Care")
+    bear = Bear.new("Care", "Fictional")
     assert_equal("Care", bear.name)
+  end
 
+  def test_get_bear_type
+    bear = Bear.new("Care", "Fictional")
+    assert_equal("Fictional", bear.type)
   end
 
 end
